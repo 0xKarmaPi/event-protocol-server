cfaaeac7952748829e883a14ae9349505948af9a
goni098 <mtheanh2910@gmail.com>
2024-09-04 14:41:34 +0700

    change program id

--- crates/program/src/lib.rs
+++ crates/program/src/lib.rs
@@ -8,7 +8,7 @@ use error::ProgramError;
 use solana_client::nonblocking::rpc_client::RpcClient;
 use solana_sdk::pubkey::Pubkey;
 
-pub const PROGRAM_ID_STR: &str = "3QBHFR3hQrqkuHcgFsxoQB4wta7uKQoDHe4Fg8Bk1q8N";
+pub const PROGRAM_ID_STR: &str = "DkrbiiPVcc1enxhZKo3Vf3jcmt15EkNBauC7rY8EeFyq";
 
 pub async fn deserialize_account<T: BorshDeserialize>(
     client: &RpcClient,
